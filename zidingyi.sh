293c468525c4acb705a0b5581a7c3dec1a4a6d45
cooip-jm <78735965+cooip-jm@users.noreply.github.com>
2021-02-09 18:26:50 +0800

    Update zidingyi.sh

--- zidingyi.sh
+++ zidingyi.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #批量替换版本号，手动运行
-sed -i "s/android@8.0100/android@8.0102/g" -rl /AutoSignMachine/commands/tasks/unicom/*.js
-sed -i "s/android\%408.0100/android\%408.0102/g" -rl /AutoSignMachine/commands/tasks/unicom/*.js
+sed -i "s/android@8.0100/android@8.0102/g" -r /AutoSignMachine/commands/tasks/unicom/*.js
+sed -i "s/android\%408.0100/android\%408.0102/g" -r /AutoSignMachine/commands/tasks/unicom/*.js
+sed -i "s/8.0100/8.0102/g" -r /AutoSignMachine/commands/tasks/unicom/*.js
+sed -i "s/8.0100/8.0102/g" -r /AutoSignMachine/utils/util.js
+
